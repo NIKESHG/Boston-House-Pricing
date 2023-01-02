b0aea14312d0cc6361adfca678b8488d3d3a6d47
NIKESH G <gotalnikesh322@gmail.com>
2023-01-02 21:46:04 +0530

    update

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    scaler=StandardScaler()
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
@@ -25,7 +26,7 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
