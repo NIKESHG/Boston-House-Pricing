6a8f1cdebd9693508706661660239e98160795f4
NIKESH G <gotalnikesh322@gmail.com>
2023-01-02 21:50:30 +0530

    commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import pickle
-from sklearn.preprocessing import StandardScaler
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
@@ -12,12 +11,12 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 def home():
     return render_template('home.html')
 
+
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    scaler=StandardScaler()
     new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
